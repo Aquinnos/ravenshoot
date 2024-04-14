86b48a98b5a3d544439cee9de6af891e891a9362
Aquinnos <wellandcomeu@gmail.com>
2024-04-14 17:14:09 +0200

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,13 @@
-export default {
-    base: '/point-shoot_game/', // Change '<REPO_NAME>' to 'point-shoot_game'
-}
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // Ustawia ścieżkę bazową dla projektu na GitHub Pages
+  base: '/ravenshoot/',
+  build: {
+    // Konfiguracja dla budowy, dostosuj według potrzeb
+    outDir: 'dist',  // domyślnie Vite zapisuje pliki do katalogu 'dist'
+    // Dalsze opcje konfiguracyjne budowy
+  }
+  // Możesz dodać więcej opcji konfiguracji zależnie od potrzeb projektu
+});
