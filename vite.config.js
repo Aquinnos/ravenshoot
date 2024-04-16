33816e6489690840408e6733e0e70e16d046315c
Aquinnos <wellandcomeu@gmail.com>
2024-04-16 21:14:42 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // vite.config.js
 import { defineConfig } from 'vite';
 
@@ -12,18 +11,3 @@ export default defineConfig({
   }
   // Możesz dodać więcej opcji konfiguracji zależnie od potrzeb projektu
 });
-=======
-// vite.config.js
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  // Ustawia ścieżkę bazową dla projektu na GitHub Pages
-  base: '/ravenshoot/',
-  build: {
-    // Konfiguracja dla budowy, dostosuj według potrzeb
-    outDir: 'dist',  // domyślnie Vite zapisuje pliki do katalogu 'dist'
-    // Dalsze opcje konfiguracyjne budowy
-  }
-  // Możesz dodać więcej opcji konfiguracji zależnie od potrzeb projektu
-});
->>>>>>> a56c0d778b61b9927d4437adf12c2c68a7e86f99
